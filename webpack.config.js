f1aaadbc975c9b58d37cb9539cb0d6f84143caa2
lionrock <xunli.xyz@gmail.com>
2016-08-21 14:45:03 +0800

    remove devtool for webpack eval

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 
 
 module.exports = {
-    devtool: 'eval',    // 会显示出报错行数与文件名, 等效命令: webpack --devtool eval
+    //devtool: 'eval',    // webpack --devtool eval
     entry: {
         app: './App/index.js'
     },

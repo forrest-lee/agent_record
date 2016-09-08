698bfa4b0657031b486f8359dc93c3cce285c90d
lionrock <xunli.xyz@gmail.com>
2016-09-08 23:13:31 +0800

    fixed

--- App/components/agency/show.js
+++ App/components/agency/show.js
@@ -43,7 +43,7 @@ const columns = [{
     title: '上级代理',
     dataIndex: 'parent',
     key: 'parent',
-    render: (value, record) => record.parentId.role !=0 ? <span>{record.parentId.name}</span> : <span>无</span>
+    render: (value, record) => record.role !=0 ? <span>{record.parentId.name}</span> : <span>无</span>
 }, {
     title: '子代理',
     render: (value, record) => {
